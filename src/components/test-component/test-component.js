7951099994fc77a237745562f7d101425d45c7a0
George Treviranus <3946669+geotrev@users.noreply.github.com>
2019-10-28 13:31:40 -0700

    Update test-component.js

--- src/components/test-component/test-component.js
+++ src/components/test-component/test-component.js
@@ -1,31 +1,32 @@
 import styles from "./styles.scss"
 
-class TestComponent extends HTMLElement {
-  constructor() {
-    super()
-    this.shadow = this.attachShadow({ mode: "open" })
-    this.setStyles()
-    this.render()
-  }
-
-  setStyles() {
-    const style = document.createElement("style")
-    style.textContent = styles
-    this.shadow.appendChild(style)
-  }
-
-  render() {
-    const wrapper = document.createElement("p")
-    const name = this.getAttribute("name")
-
-    wrapper.innerHTML = `
-      <div class="some-class">Hi, I'm ${name}!</div>
-    `
-
-    this.shadow.appendChild(wrapper)
-  }
-}
-
 if (typeof window !== "undefined") {
-  customElements.define("tails-test", TestComponent)
+  customElements.define(
+    "tails-test", 
+    class extends HTMLElement {
+      constructor() {
+        super()
+        this.shadow = this.attachShadow({ mode: "open" })
+        this.setStyles()
+        this.render()
+      }
+
+      setStyles() {
+        const style = document.createElement("style")
+        style.textContent = styles
+        this.shadow.appendChild(style)
+      }
+
+      render() {
+        const wrapper = document.createElement("p")
+        const name = this.getAttribute("name")
+
+        wrapper.innerHTML = `
+          <div class="some-class">Hi, I'm ${name}!</div>
+        `
+
+        this.shadow.appendChild(wrapper)
+      }
+    }
+  )
 }
